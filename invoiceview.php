11dc6e3749862fc6857898354ea442fd6cf5c40b
ProjectHBK <34718421+projecthbk@users.noreply.github.com>
2020-04-03 11:53:24 +0200

    Add files via upload

--- invoiceview.php
+++ invoiceview.php
@@ -36,18 +36,18 @@
 		$info = mysqli_fetch_array($result);
 		mysqli_free_result($result);
 		$html = '<body>
-	<div style="text-align: center;"><img class="logo" src="../logo/' . $_GET['company'] . '.png" /></div>
-	<table class="top"><tr><td style="background: lightgray;">Faktura</td><td style="background: #e7e7e8;">' . $row['invoiceid'] . '</td><td></td><td></td><td style="background: lightgray;">Data</td><td style="background: #e7e7e8;">' . $row['creation'] . '</td></table>
-	<table class="main"><tr><th style="background: ' . $info['color'] . ';">Dane firmy:</th><th style="background: ' . $info['color'] . ';"></th><th style="background: ' . $info['color'] . ';"></th></tr>
+	<div style="text-align: center;"><img class="logo" src="logo/' . $_GET['company'] . '.png" /></div>
+	<table class="top"><tr><td style="background: lightgray;">Receipt</td><td style="background: #e7e7e8;">' . $row['invoiceid'] . '</td><td></td><td></td><td style="background: lightgray;">Date</td><td style="background: #e7e7e8;">' . $row['creation'] . '</td></table>
+	<table class="main"><tr><th style="background: ' . $info['color'] . ';">Payee Name:</th><th style="background: ' . $info['color'] . ';"></th><th style="background: ' . $info['color'] . ';"></th></tr>
 	<tr><td>' . $info['display'] . '<br />' . str_replace("\n","<br />",$info['address']) . '</td>
-	<td><strong>KONTAKT:</strong><br />' . str_replace("\n","<br />",$info['contact']) . '</td>
-	<td style="width: 45%;"><strong>BANK:</strong><br />' . str_replace("\n","<br />",$row['bank']) . '</td>
+	<td><strong>CONTACT:</strong><br />' . str_replace("\n","<br />",$info['contact']) . '</td>
+	<td style="width: 45%;"><strong>BANK DETAILS:</strong><br />' . str_replace("\n","<br />",$row['bank']) . '</td>
 	</table>
-	<table class="main"><tr><th style="background: ' . $info['color'] . ';">Dane płatnika:</th><th style="background: ' . $info['color'] . ';"></th><th style="background: ' . $info['color'] . ';"></th></tr>
+	<table class="main"><tr><th style="background: ' . $info['color'] . ';">Payer Name:</th><th style="background: ' . $info['color'] . ';"></th><th style="background: ' . $info['color'] . ';"></th></tr>
 	<tr><td>' . nl2br($row['clientinfo']);
-	if ($client['nip']!='') $html .= '<br />NIP: ' . $client['nip'];
+	if ($client['nip']!='') $html .= '<br />TIN: ' . $client['nip'];
 	$html .=  '</td><td><strong>';
-	if ($client['mail']!='' || $client['mobile']!='') $html .= 'KONTAKT:';
+	if ($client['mail']!='' || $client['mobile']!='') $html .= 'CONTACT:';
 	$n=0;
 	foreach(preg_split('~[\n]+~', $row['brutto']) as $line){
 		if ($line==0) $brutto[$n] = 0; else $brutto[$n] = $line; 
@@ -73,7 +73,7 @@
 	$html .= '</strong><br />' . $client['mail'] . '<br />' . $client['mobile'] . '</td>
 	<td style="width: 45%;"></td>
 	</table>
-	<table class="main description"><tr><th style="background: ' . $info['color'] . '; width: 6%;">Lp.</th><th style="background: ' . $info['color'] . '; width: 35%;">Opis</th><th style="background: ' . $info['color'] . '; width: 15%;">Typ</th><th style="background: ' . $info['color'] . '; width: 13%;">Netto</th><th style="background: ' . $info['color'] . '; width: 8%;">%</th><th style="background: ' . $info['color'] . '; width: 10%;">VAT</th><th style="background: ' . $info['color'] . '; width: 13%;">Brutto</th></tr>
+	<table class="main description"><tr><th style="background: ' . $info['color'] . '; width: 6%;">No.</th><th style="background: ' . $info['color'] . '; width: 35%;">Description</th><th style="background: ' . $info['color'] . '; width: 15%;">Type</th><th style="background: ' . $info['color'] . '; width: 13%;">Netto</th><th style="background: ' . $info['color'] . '; width: 8%;">%</th><th style="background: ' . $info['color'] . '; width: 10%;">VAT</th><th style="background: ' . $info['color'] . '; width: 13%;">Brutto</th></tr>
 	<tr><td style="text-align: center;">';
 	$n=0;
 	foreach(preg_split('~[\n]+~', $row['description']) as $line){
@@ -83,18 +83,18 @@
     $html .= '</td><td>' . nl2br($row['description']) . '</td>
 	<td style="height: 70mm;">' . nl2br($row['type']) . '</td><td class="right">' . $netto . '</td><td>' . nl2br($row['vat']) . '</td>';
 	$html .= '<td class="right">' . $vatvalue . '</td><td class="right">' . $row['brutto'] . '</td></tr></table>
-	<table class="main"><tr><th style="background: ' . $info['color'] . ';">Dodatkowe informacje</td></tr><tr><td style="height: 20mm;">' . $row['info'] . '</td></tr></table>
-	<table class="top"><tr><td></td><td></td><td style="background: ' . $info['color'] . '; color: #ffffff;">PODATEK</td><td style="background: #e7e7e8;">';
+	<table class="main"><tr><th style="background: ' . $info['color'] . ';">Additional information</td></tr><tr><td style="height: 20mm;">' . $row['info'] . '</td></tr></table>
+	<table class="top"><tr><td></td><td></td><td style="background: ' . $info['color'] . '; color: #ffffff;">TAX</td><td style="background: #e7e7e8;">';
 	if ($row['currency']=='USD') $html .= '$'; if ($row['currency']=='EUR') $html .= '€'; if ($row['currency']=='GBP') $html .= '£';
 	$html .= number_format($podatek,2);
 	if ($row['currency']=='PLN') $html .= ' zł';
-	$html .='</td><td style="background: ' . $info['color'] . '; color: #ffffff;">DO ZAPŁATY</td><td class="right" style="background: #e7e7e8;">';
+	$html .='</td><td style="background: ' . $info['color'] . '; color: #ffffff;">TOTAL</td><td class="right" style="background: #e7e7e8;">';
 	if ($row['currency']=='USD') $html .= '$'; if ($row['currency']=='EUR') $html .= '€'; if ($row['currency']=='GBP') $html .= '£';
 	$html .= number_format($row['total'],2);
 	if ($row['currency']=='PLN') $html .= ' zł';
 	$html .= '</td></tr></table>
 	<div id="footer" style="text-align: center;">
-    Ten dokument stanowi oryginalną wersję elektroniczną.
+    This document is original electronic version.
 	</div>
 	</div>
 	</body>';
