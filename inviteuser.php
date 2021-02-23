37d24668aad81796fb8a6f4ca78e4d51dc76c23a
ZygTech.pl <34718421+zygtech@users.noreply.github.com>
2021-02-23 08:27:50 +0100

    Update inviteuser.php

--- inviteuser.php
+++ inviteuser.php
@@ -33,7 +33,7 @@
 		die('Mail already used.');
 	$header = 'From: Goldwater <krzysztof@zygtech.pl>';
 	$content = $url . '/setpassword.php?company=' . $_SESSION['company'] . '&login=' . $_POST['login'] . '&mail=' . $_POST['mail'] . '&code=' . md5($_SESSION['company'] . $_POST['mail'] . $_POST['login'] . 'goldwater_pass_check');
-	if ($_SESSION['login']!='' && $_SESSION['company']!='goldwater') 
+	if ($_SESSION['login']!='') 
 		mail($_POST['mail'],$_SESSION['company'] . ' - set your password: ' . $_POST['login'],$content,$header);
 	mysqli_close($link);
 ?>
